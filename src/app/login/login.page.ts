cbd14532a1a6c5612525aeb7059124d903899db0
yonykikok <jonathan.n.haedo@gmail.com>
2020-05-24 12:48:28 -0300

    login final (estetico)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { User } from '../clases/user';
 import { AuthService } from '../services/auth.service';
 import { ToastService } from '../services/toast.service';
 import { Vibration } from '@ionic-native/vibration/ngx';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-login',
@@ -11,24 +12,34 @@ import { Vibration } from '@ionic-native/vibration/ngx';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  esLogin: boolean = false;
   @Input() user: User = new User();
   @Input() listaUsuarios = [
+    { "email": "supervisor@gmail.com", "password": "supervisor" },
     { "email": "mozo@gmail.com", "password": "mozomozo" },
-    { "email": "admin@gmail.com", "password": "adminadmin" },
-    { "email": "bartender@gmail.com", "password": "bartender" },
     { "email": "cocinero@gmail.com", "password": "cocinero" },
-    { "email": "usuario@gmail.com", "password": "usuario" }]
+    { "email": "bartender@gmail.com", "password": "bartender" },
+    { "email": "cliente@gmail.com", "password": "cliente" },
+    { "email": "admin@gmail.com", "password": "adminadmin" },
+  ]
 
 
   constructor(
     private authService: AuthService,
     private toast: ToastService,
     private router: Router,
-    private vibration:Vibration) { }
+    private dataBase: DatabaseService, ) { }
 
   ngOnInit() {
   }
+  onLogin2() {//cambiar el sistema de registro y login por usuarios sin email.
+    this.dataBase.obtenerTodos('usuarios').subscribe(listaDeUsuarios => {
+      listaDeUsuarios.forEach(usuario => {
 
+        console.log(usuario);
+      });
+    });
+  }
   async onLogin() {
     const response = await this.authService.onLogin(this.user);
     if (response.user) {
